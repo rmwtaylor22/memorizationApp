95eda237681c055f366011e7be90fb74864809af
rwhite <rose_white@taylor.edu>
2021-03-21 19:14:19 -0400

    I am so close to having this complete. I'm still getting errors about the cursor in my db.py file and my login path is recieving a 405 error whenever I submit my login info.

--- db.py
+++ db.py
@@ -2,8 +2,7 @@ from flask import g
 import psycopg2
 import psycopg2.extras
 
-
-data_source_name = "dbname=rwhite user=rwhite password=rwhite host=roller.cse.taylor.edu"
+data_source_name = "dbname=pika user=rwhite password=rwhite host=roller.cse.taylor.edu"
 
 
 def open_db_connection():
@@ -16,12 +15,23 @@ def close_db_connection():
     g.connection.close()
 
 
-def trip_report():
+def find_member(memberEmail):
     query = """
-        SELECT s.first_name, s.last_name,  s.class, t.destination, t.year, t.semester
-        FROM student AS s
-           INNER JOIN student_trip AS st ON s.id = st.student_id 
-           INNER JOIN trip AS t ON st.trip_id = t.id
-        """
-    g.cursor.execute(query)
-    return g.cursor.fetchall()
+    SELECT m.email, m.first_name, m.last_name, p.file_path
+    FROM member AS m
+       LEFT OUTER JOIN photo AS p ON m.email = p.member_email 
+    WHERE email = %(emailParam)s
+    """
+    g.cursor.execute(query, {'emailParam': memberEmail})
+    return g.cursor.fetchone()
+
+
+def create_member(email, first_name, last_name, password):
+    query = '''
+    INSERT INTO member (email, first_name, last_name, password)
+    VALUES (%(email)s, %(first)s, %(last)s, %(pass)s)
+    '''
+
+    g.cursor.execute(query, {'email': email, 'first': first_name, 'last': last_name, 'pass': password})
+    g.connection.commit()
+    return g.cursor.rowcount
--- forms.py
+++ forms.py
@@ -3,6 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import Email, Length, Regexp, EqualTo
 
+import db  # if error, right-click parent directory "mark directory as" "sources root"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key for session application'
 
@@ -10,12 +12,12 @@ app.config['SECRET_KEY'] = 'secret key for session application'
 class LoginForm(FlaskForm):
     valid_pword = []
     valid_pword.append(Length(min=8))
-    valid_pword.append(Regexp(r'.*[A-Za-z]', message="Must have at least one letter"))
-    valid_pword.append(Regexp(r'.*[0-9]', message="Must have at least one digit"))
-    valid_pword.append(Regexp(r'.*[!@#$%^&*_+=]', message="Must have at least one special character"))
+    valid_pword.append(Regexp(r'.*[A-Za-z]', message="Password must have at least one letter"))
+    valid_pword.append(Regexp(r'.*[0-9]', message="Password must have at least one digit"))
+    valid_pword.append(Regexp(r'.*[!@#$%^&*_+=]', message="Password must have at least one special character"))
 
     long = []
-    long.append(Length(min=5))
+    long.append(Length(min=4))
 
     firstName = StringField('First Name', validators=long)
     lastName = StringField('Last Name', validators=long)
@@ -33,13 +35,13 @@ def authenticate_user(fname, lname, email, pword):
     if len(lname) < 5:
         return False
 
-    fake_user_database = {}
-    fake_user_database['ds@cse.taylor.edu'] = 'Pa$$word123'
-    fake_user_database['bnroller@taylor.edu'] = 'Gr3atteach!'
+    ## fake_user_database = {}
+    ## fake_user_database['ds@cse.taylor.edu'] = 'Pa$$word123'
+    ## fake_user_database['bnroller@taylor.edu'] = 'Gr3atteach!'
 
-    if email in fake_user_database:
-        if fake_user_database[email] == pword:
-            return True
+    # if email in db.login(email):
+    #    if db[email] == pword:  ## how do I do this?
+    #        return True
 
     return False
 
@@ -51,35 +53,62 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # provide user a login form
     register_form = LoginForm()
 
+    # if the info is valid
     if register_form.validate_on_submit():
-        ##if not authenticate_user(register_form.firstName.data, register_form.lastName.data, register_form.email.data, register_form.password.data):
-        ##   flash('There are errors in your registration form:')
-        ##else:
-        session['firstName'] = register_form.firstName.data
-        session['email'] = register_form.email.data
+        member = db.find_member(register_form.email.data)
+
+        if member is not None:
+            flash("Member {} already exists".format(register_form.email.data))
+        else:
+            rowcount = db.create_member(register_form.email.data,
+                                        register_form.first_name.data,
+                                        register_form.last_name.data,
+                                        register_form.password.data)
+
+            if rowcount == 1:
+                flash("Member {} created".format(register_form.email.data))
+                return redirect(url_for('index'))
+            else:
+                flash("New member not created")
+            # fill the session in with the details
+            session['firstName'] = register_form.firstName.data
+            session['lastName'] = register_form.lastName.data
+            session['email'] = register_form.email.data
+            session['password'] = register_form.password.data
 
         flash('User registered')
         return redirect(url_for('confirmation'))
 
     return render_template('register.html', form=register_form)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    register_form = LoginForm()
 
-    if register_form.validate_on_submit():
-        ##if not authenticate_user(register_form.firstName.data, register_form.lastName.data, register_form.email.data, register_form.password.data):
-        ##   flash('There are errors in your registration form:')
-        ##else:
-        session['firstName'] = register_form.firstName.data
-        session['email'] = register_form.email.data
+# @app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    # if the info is valid
+    if login_form.validate_on_submit():
+        member = db.find_member(login_form.email.data)
+
+        if member is None:
+            flash("Member {} does not exist".format(login_form.email.data))
+        else:
+            # fill the session in with the details
+            session['firstName'] = login_form.firstName.data
+            session['lastName'] = login_form.lastName.data
+            session['email'] = login_form.email.data
+            session['password'] = login_form.password.data
+
+            flash("Member {} logged in".format(login_form.email.data))
+            return redirect(url_for('index'))
 
         flash('User logged in')
         return redirect(url_for('confirmation'))
 
-    return render_template('login.html', form=register_form)
+    return render_template('login.html', form=login_form)
 
 
 @app.route('/logout')
